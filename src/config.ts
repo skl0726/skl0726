88a216381493e44c75fbd06ee954c50300a64682
이세광 <sekwang@p132-n38.kthopen.kth.se>
2026-03-17 12:20:09 +0100

    fix config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Sekwang Lee | 이세광';
-// export const SITE_DESCRIPTION = 'Astrofy is a free and open-source template for your Personal Portfolio Website built with Astro and TailwindCSS. Create in minutes a website with Blog, CV, Project Section, Store and RSS Feed.';
+export const SITE_DESCRIPTION = 'Portfolio website of Sekwang Lee';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
